3cbdafd6d953cdbbab2dc6b26597819340323019
latif <mabdullatifm1127@gmail.com>
2026-03-08 01:38:02 +0700

    Langkah 6

---
+++ src/pages/utlis/db/firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrBOlFYk9Qhcz4jWxwVnX-DYwi2qMHp98",
+  authDomain: "framework-next-81cc4.firebaseapp.com",
+  projectId: "framework-next-81cc4",
+  storageBucket: "framework-next-81cc4.firebasestorage.app",
+  messagingSenderId: "271492418382",
+  appId: "1:271492418382:web:58195848fa72efec30ec1f"
+};
