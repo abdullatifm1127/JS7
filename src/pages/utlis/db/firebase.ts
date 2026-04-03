a613b175113fe33abf4824af628fea9c76012cbd
latif <mabdullatifm1127@gmail.com>
2026-04-03 22:52:08 +0700

    js7

--- src/pages/utlis/db/firebase.ts
+++ src/pages/utlis/db/firebase.ts
@@ -1,5 +1,9 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBrBOlFYk9Qhcz4jWxwVnX-DYwi2qMHp98",
   authDomain: "framework-next-81cc4.firebaseapp.com",
@@ -9,6 +13,7 @@ const firebaseConfig = {
   appId: "1:271492418382:web:58195848fa72efec30ec1f"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
